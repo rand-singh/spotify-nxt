7843c1444d1c9677b493d8cdfe991b4230405f6c
Randeep Singh <rand-singh@users.noreply.github.com>
2022-01-02 13:26:23 +0000

    Setup spotify api

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Spotify Next JS</h1>
-
-      <main>
-        {/* sidebar */}
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="">
+        <Sidebar />
         {/* center */}
       </main>
 
       <div>
         {/* player */}
       </div>
-
     </div>
   )
 }
