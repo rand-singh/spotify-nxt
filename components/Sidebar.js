a1632583227104cd887d85bd3367945a4a1aff8c
Randeep Singh <rand-singh@users.noreply.github.com>
2022-01-03 21:41:48 +0000

    Build center component
    Install lodash and tailwind scrollbar hide

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -15,7 +15,7 @@ function Sidebar() {
     console.log(session)
 
     return (
-        <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+        <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
             <div className="space-y-4">
                 <button className="flex items-center space-x-2 hover:text-white"
                     onClick={() => signOut()}>
@@ -53,6 +53,14 @@ function Sidebar() {
                 <hr className="border-t-[0.1px] border-gray-900"/>
 
                 {/* Playlists */}
+                <p className="cursor-pointer hover:text-white">Playlist name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist name...</p>
                 <p className="cursor-pointer hover:text-white">Playlist name...</p>
                 <p className="cursor-pointer hover:text-white">Playlist name...</p>
                 <p className="cursor-pointer hover:text-white">Playlist name...</p>
